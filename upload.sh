a0b9496511111b247467e9965959ff98691ed58c
300481 <300481@300481.de>
2019-06-09 14:35:20 +0200

    replace Server IP, GW and DNS by variable

--- upload.sh
+++ upload.sh
@@ -4,6 +4,8 @@
 
 CUSTOMER=$1
 
+export GW=$(ip route | awk '/default/ { print $3 }')
+export IP=$(ip route | awk '/default/ { print $9 }')
 export PUBKEY=$(< ~/.ssh/id_rsa.pub)
 
 curl -X POST -d "$(envsubst < ${CUSTOMER}/config.yaml)" localhost:8080/api/config
