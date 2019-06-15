11db5cc4b3e4aeb1bf9ce7aaa8c502b17b74bbe6
300481 <300481@300481.de>
2019-06-15 23:22:52 +0200

    check if app is running

--- init.sh
+++ init.sh
@@ -4,7 +4,18 @@
 
 CUSTOMER=$1
 
+# get apps version for this customer config
 APPVER=$(docker run -i --rm --name yq 300481/yq:v2.4.0 appver < ${CUSTOMER}/config.yaml)
 
+# clone app and start
 git clone --branch ${APPVER} git@github.com:300481/3141.git app
 app/3141.sh
+
+# wait for app
+for x in $(seq 10) ; do
+    APPSTATUS=$(docker inspect -f '{{ .State.Status }}' 3141)
+    DNSSTATUS=$(docker inspect -f '{{ .State.Status }}' dnsmasq)
+    [[ "${APPSTATUS}" == "running" ]] && [[ "${DNSSTATUS}" == "running" ]] && break
+    [[ ${x} -eq 10 ]] && echo "app not running" && exit 1
+    sleep 2
+done
