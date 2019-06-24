defe8e93c7db86a26818da5111595182f2fdadac
300481 <300481@300481.de>
2019-06-24 12:48:00 +0200

    add needed templates, change upload script

--- upload.sh
+++ upload.sh
@@ -4,8 +4,20 @@
 
 CUSTOMER=$1
 
-export GW=$(ip route | awk '/default/ { print $3 }')
-export IP=$(ip route | awk '/default/ { print $9 }')
-export PUBKEY=$(< ~/.ssh/id_rsa.pub)
+uploadTemplates() {
+    cd ${CUSTOMER}
+    for TEMPLATE in $(find api -type f) ; do
+        curl -X POST -d "$(< ${TEMPLATE})" localhost:8080/${TEMPLATE}
+    done
+    cd ..
+}
 
-curl -X POST -d "$(envsubst < ${CUSTOMER}/config.yaml)" localhost:8080/api/config
+uploadConfig() {
+    export GW=$(ip route | awk '/default/ { print $3 }')
+    export IP=$(ip route | awk '/default/ { print $9 }')
+    export PUBKEY=$(< ~/.ssh/id_rsa.pub)
+    curl -X POST -d "$(envsubst < ${CUSTOMER}/config.yaml)" localhost:8080/api/config
+}
+
+uploadTemplates
+uploadConfig
