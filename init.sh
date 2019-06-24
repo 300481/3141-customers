53cc8a9f7c9357b1cb4e6954e17849c429282747
300481 <300481@300481.de>
2019-06-24 17:33:14 +0200

    add ssh key generation

--- init.sh
+++ init.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-[[ -n $1 ]] || { echo "usage: $0 [CUSTOMER] [update,start]" ; exit 1 ; }
+[[ -n $1 ]] || { echo "usage: $0 [CUSTOMER] [update,start,keygen]" ; exit 1 ; }
 
 CUSTOMER=${1}
 MODE=${2:-start}
+SSHDIR=${CUSTOMER}/.ssh
 
 startApp() {
     export APPVER=$(docker run -i --rm --name yq 300481/yq:v2.4.0 appver < ${CUSTOMER}/config.yaml)
     export DNSVER=$(docker run -i --rm --name yq 300481/yq:v2.4.0 dnsver < ${CUSTOMER}/config.yaml)
     docker-compose up -d
+}
+
+keyGen() {
+    if [[ -f ${CUSTOMER}/.ssh/id_rsa ]] ; then
+        exit
+    fi
+    if ! [[ -d ${SSHDIR} ]] ; then
+        mkdir -p ${SSHDIR}
+        chmod 700 ${SSHDIR}
+    fi
+    ssh-keygen -t rsa -b 4096 -f ${SSHDIR}/id_rsa -P "" -C "" -q
+    exit
 }
 
 uploadTemplates() {
@@ -22,7 +35,7 @@ uploadTemplates() {
 uploadConfig() {
     export GW=$(ip route | awk '/default/ { print $3 }')
     export IP=$(ip route | awk '/default/ { print $9 }')
-    export PUBKEY=$(< ~/.ssh/id_rsa.pub)
+    export PUBKEY=$(< ${SSHDIR}/id_rsa.pub)
     curl -X POST -d "$(envsubst < ${CUSTOMER}/config.yaml)" localhost:8080/api/config
 }
 
@@ -36,6 +49,10 @@ if [[ "${MODE}" == "start" ]] ; then
     startApp
 fi
 
-uploadTemplates
-uploadConfig
-uploadClusterConfig
+if [[ "${MODE}" == "keygen" ]]; then
+    keyGen
+else
+    uploadTemplates
+    uploadConfig
+    uploadClusterConfig
+fi
