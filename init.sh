e401304f6019afe6fcf91554bc0eab5a09972ced
300481 <300481@300481.de>
2019-06-24 13:14:32 +0200

    use compose-file for start

--- init.sh
+++ init.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
-[[ -n $1 ]] || { echo "usage: $0 [CUSTOMER]" ; exit 1 ; }
+[[ -n $1 ]] || { echo "usage: $0 [CUSTOMER] [update,start]" ; exit 1 ; }
 
-CUSTOMER=$1
+CUSTOMER=${1}
+MODE=${2:-start}
 
-# get apps version for this customer config
-APPVER=$(docker run -i --rm --name yq 300481/yq:v2.4.0 appver < ${CUSTOMER}/config.yaml)
+startApp() {
+    export APPVER=$(docker run -i --rm --name yq 300481/yq:v2.4.0 appver < ${CUSTOMER}/config.yaml)
+    export DNSVER=$(docker run -i --rm --name yq 300481/yq:v2.4.0 dnsver < ${CUSTOMER}/config.yaml)
+    docker-compose up
+}
 
-# clone app and start
-git clone --branch ${APPVER} https://github.com/300481/3141.git app
-app/3141.sh
+uploadTemplates() {
+    cd ${CUSTOMER}
+    for TEMPLATE in $(find api -type f) ; do
+        curl -X POST -d "$(< ${TEMPLATE})" localhost:8080/${TEMPLATE}
+    done
+    cd ..
+}
 
-# wait for app
-for x in $(seq 10) ; do
-    APPSTATUS=$(docker inspect -f '{{ .State.Status }}' 3141)
-    DNSSTATUS=$(docker inspect -f '{{ .State.Status }}' dnsmasq)
-    [[ "${APPSTATUS}" == "running" ]] && [[ "${DNSSTATUS}" == "running" ]] && break
-    [[ ${x} -eq 10 ]] && echo "app not running" && exit 1
-    sleep 2
-done
+uploadConfig() {
+    export GW=$(ip route | awk '/default/ { print $3 }')
+    export IP=$(ip route | awk '/default/ { print $9 }')
+    export PUBKEY=$(< ~/.ssh/id_rsa.pub)
+    curl -X POST -d "$(envsubst < ${CUSTOMER}/config.yaml)" localhost:8080/api/config
+}
+
+if ( "${MODE}" == "start" ) ; then
+    startApp
+fi
+
+uploadTemplates
+uploadConfig
